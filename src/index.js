d4b043d1f00fdf1fd5488d5b20b9940c61e47818
AdrianSiwek <105585774+AdrianSiwek@users.noreply.github.com>
2022-10-17 17:15:03 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import './css/styles.css';
 import Notiflix from 'notiflix';
 import debounce from 'lodash.debounce';
+import { fetchCountries } from './js/fetchCountries';
 
+const inputCountry = document.querySelector('input#search-box');
+const ulCountry = document.querySelector('country-list');
+const divCountry = document.querySelector('country-info');
 
 const DEBOUNCE_DELAY = 300;
