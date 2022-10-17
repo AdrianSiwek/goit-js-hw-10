1e952c89556359470e822ae4202ad8f85e883427
AdrianSiwek <105585774+AdrianSiwek@users.noreply.github.com>
2022-10-17 16:31:35 +0200

    Create fetchCountries.js

---
+++ src/js/fetchCountries.js
@@ -0,0 +1,5 @@
+const COUNTRIES_API_URL = 'https://restcountries.com/#api-endpoints-v3-name';
+
+const fetchCountries = (designation) => {
+    fetch(`${COUNTRIES_API_URL}${designation}?fields = name, capital, population, flags, languages`).then((response) => response.json);
+}
