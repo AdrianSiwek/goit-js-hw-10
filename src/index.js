117a8ca8530b14098fa4e336e88498d276f9dc63
AdrianSiwek <105585774+AdrianSiwek@users.noreply.github.com>
2023-01-05 00:30:52 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,13 +9,24 @@ const divCountry = document.querySelector('.country-info');
 
 const DEBOUNCE_DELAY = 300;
 
-function createListMarkup(data) {
+
+const createCountryInfo = country => {
+    const li = document.createElement('li');
+    li.innerHTML = `<div class="inormation-container"><img src=${country.flags.svg} /><p class="country-name">${country.name.official}</p><ul><li><p class="country-info">Population: ${country.population}</p></li><li><p class="country-info">Capital city: ${country.capital}</p></li><li><p class="country-info">Languages: ${Object.values(country.languages).join(', ')}</p></li></ul></div>`
+    ulCountry[0].append(li);
+};
+
+function createInfoMarkup(data) {
   return data
     .map(
       ({ name, flags }) =>
-        `<li><img src="${flags.svg}" alt="${name.common}" width="40" height="30"/>${name.common}</li>`
+        `<a href="http://wikipedia.org/wiki/${name.common}" target="_blank"><li class="list-item" data-name="${name.common}"><img src="${flags.svg}" alt="${name.common}"/>${name.common}</li></a>`
     )
     .join('');
+}
+
+const removeCountry = () => {
+    while (ulCountry.firstChild) ulCountry.removeChild(ulCountry.firstChild);
 }
 
 const onFetchCountries = async (event) => {
@@ -25,9 +36,17 @@ const onFetchCountries = async (event) => {
     if (countriesResp.status === 404) {
         Notify.warning("Oops, there is no country with that name");
     } else if (countriesResp.status === 200) {
-        if (countries.length > 10) {
+        if (countries && countries.length > 10) {
             Notify.info("Too many matches found. Please enter a more specific name.");
+        } else if (countries && countries.length === 1) {
+            removeCountry();
+            countries.forEach(country => createCountryInfo(country));
+        } else if (countries && countries.length >= 2 && countries.length <= 10) {
+            ulCountry[0].textContent = createInfoMarkup(countries);
         }
+    }
+    if (event.target.value.trim() === '') {
+        removeCountry();
     }
 }
     
@@ -38,4 +57,3 @@ const onFetchCountries = async (event) => {
 
 inputCountry.addEventListener('input', debounce(onFetchCountries, DEBOUNCE_DELAY));
 
-console.log(123);
