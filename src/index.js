9b362ef654e0d2b5cfb97d6e18a9d2fc9caed40b
AdrianSiwek <105585774+AdrianSiwek@users.noreply.github.com>
2023-01-04 23:30:50 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,13 +4,22 @@ import debounce from 'lodash.debounce';
 import { fetchCountries } from './js/fetchCountries';
 
 const inputCountry = document.querySelector('input#search-box');
-const ulCountry = document.querySelector('country-list');
-const divCountry = document.querySelector('country-info');
+const ulCountry = document.querySelector('.country-list');
+const divCountry = document.querySelector('.country-info');
 
 const DEBOUNCE_DELAY = 300;
 
+function createListMarkup(data) {
+  return data
+    .map(
+      ({ name, flags }) =>
+        `<li><img src="${flags.svg}" alt="${name.common}" width="40" height="30"/>${name.common}</li>`
+    )
+    .join('');
+}
+
 const onFetchCountries = async (event) => {
-    const countriesResp = await fetchCountries(event.target.value);
+    const countriesResp = await fetchCountries(event.target.value.trim());
     const countries = await countriesResp.json();
     console.log(countries);
     if (countriesResp.status === 404) {
@@ -24,14 +33,7 @@ const onFetchCountries = async (event) => {
     
 
 
-// function createListMarkup(data) {
-//   return data
-//     .map(
-//       ({ name, flags }) =>
-//         `<li><img src="${flags.svg}" alt="${name.common}" width="40" height="30"/>${name.common}</li>`
-//     )
-//     .join('');
-// }
+
 
 
 inputCountry.addEventListener('input', debounce(onFetchCountries, DEBOUNCE_DELAY));
