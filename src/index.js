71bc3de3e0fa6cb357cbdce3d251b0f44c77b658
AdrianSiwek <105585774+AdrianSiwek@users.noreply.github.com>
2023-01-06 01:32:41 +0100

    update

--- src/index.js
+++ src/index.js
@@ -3,18 +3,15 @@ import Notiflix, { Notify } from 'notiflix';
 import debounce from 'lodash.debounce';
 import { fetchCountries } from './js/fetchCountries.js';
 
-const inputCountry = document.querySelector('#search-box');
-const ulCountry = document.querySelector('.country-list');
-// const divCountry = document.querySelector('.country-info');
 
 const DEBOUNCE_DELAY = 300;
-
+const inputCountry = document.getElementById('search-box');
+const ulCountry = document.getElementsByClassName('country-list');
 
 const createCountry = country => {
-    console.log(ulCountry[0]);
     const li = document.createElement('li');
     li.innerHTML = `<div class="full-info-container"><img src=${country.flags.svg} /><span class="country-name">${country.name.official}</span><ul><li><span class="country-info">Population: ${country.population}</span></li><li><span class="country-info">Capital city: ${country.capital}</span></li><li><span class="country-info">Languages: ${Object.values(country.languages).join(', ')}</span></li></ul></div>`
-    ulCountry[0].appendChild(li);
+    ulCountry[0].append(li);
 };
 
 const createInfoMarkup = (data) => {
@@ -30,15 +27,14 @@ const removeCountry = () => {
 }
 
 
-    
-const onFetchCountries = async (event) => {
-    let values = event.target.value.trim()
-    if(values === '') {
-        const countriesResp = await fetchCountries(values);
-        if(countriesResp.status === 200){
+const onFetchCountries = async event => {
+    let value = event.target.value.trim();
+    if (value !== '') {
+        const countriesResp = await fetchCountries(value);
+        if (countriesResp.status === 200) {
             const countries = await countriesResp.json();
-            if(countries && countries.length > 10) {
-                Notify.info("Too many matches found. Please enter a more specific name.");
+            if (countries && countries.length > 10) {
+                Notiflix.Notify.info('Too many matches found. Please enter a more specific name');
             } else if (countries && countries.length === 1) {
                 removeCountry();
                 countries.forEach(country => createCountry(country));
@@ -46,13 +42,36 @@ const onFetchCountries = async (event) => {
                 ulCountry[0].innerHTML = createInfoMarkup(countries);
             }
         } else if (countriesResp.status === 404) {
-            Notify.warning("Oops, there is no country with that name");
+            Notiflix.Notify.failure('Oops, there is no country with that name');
         }
     }
-    if(values === '') {
-        removeCountry();
-    }
+    if (value === '') { removeCountry() }
 }
+
+
+    
+// const onFetchCountries = async (event) => {
+//     let values = event.target.value.trim();
+//     if(values !== '') {
+//         const countriesResp = await fetchCountries(values);
+//         if(countriesResp.status === 200){
+//             const countries = await countriesResp.json();
+//             if(countries && countries.length > 10) {
+//                 Notify.info("Too many matches found. Please enter a more specific name.");
+//             } else if (countries && countries.length === 1) {
+//                 removeCountry();
+//                 countries.forEach(country => createCountry(country));
+//             } else if (countries && countries.length >= 2 && countries.length <= 10) {
+//                 ulCountry[0].innerHTML = createInfoMarkup(countries);
+//             }
+//         } else if (countriesResp.status === 404) {
+//             Notify.warning("Oops, there is no country with that name");
+//         }
+//     }
+//     if(values === '') {
+//         removeCountry();
+//     }
+// }
 
 
 
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,9 +1,9 @@
-const COUNTRIES_API_URL = 'https://restcountries.com/v3.1/name/';
+const COUNTRIES_API_URL = 'https://restcountries.com/v3.1';
 
 const fetchCountries = (name) => {
-    // console.log(name);
+    console.log(name);
     return fetch(`${COUNTRIES_API_URL}/name/${name}?fields=name,capital,population,flags,languages`)
-        // .then((response)=>response);
+        .then((response)=>response);
 };
 
 export { fetchCountries };
